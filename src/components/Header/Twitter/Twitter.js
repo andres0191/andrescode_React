113ff09fea7242fb060fa2aa5720d85d0a9b9512
andres0191 <andres_felipe0191@hotmail.com>
2020-09-02 22:07:07 -0500

    update header and footer

--- src/components/Header/Twitter/Twitter.js
+++ src/components/Header/Twitter/Twitter.js
@@ -5,7 +5,7 @@ import {SocialMediaIconsReact} from 'social-media-icons-react';
 export default function Twitter(){
     return(
         <div>
-            <SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="5" borderStyle="inset" icon="twitter" iconColor="rgba(188,188,188,1)" backgroundColor="rgba(0,0,0,1)" iconSize="5" roundness="39%" url="https://some-website.com/my-social-media-url" size="35" />
+            <SocialMediaIconsReact borderColor="rgba(0,0,0,0.25)" borderWidth="5" borderStyle="inset" icon="twitter" iconColor="rgba(188,188,188,1)" backgroundColor="rgba(0,0,0,1)" iconSize="5" roundness="39%" url="https://twitter.com/andres0191" size="35" />
         </div>
     )
 }
