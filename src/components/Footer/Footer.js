6541111a872e73efe7f81e7b4a101044334190a5
andres0191 <andres_felipe0191@hotmail.com>
2020-09-02 23:27:09 -0500

    update image

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.scss';
-import Dev from './NavAbout/NavAbout';
+import NavAbout from './NavAbout/NavAbout';
 import Github from './NavHome/Github';
 import Linkedin from './NavPRojects/Linkedin';
 
@@ -8,8 +8,8 @@ export default function Footer(){
     return(
         <div>
             <div className='foot dvfooter'>
+                <li className='space'><NavAbout /></li>
                 <li className='space'><Github /></li>
-                <li className='space'><Dev /></li>
                 <li className='space'><Linkedin /></li>
             </div>
         </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,14 +9,14 @@ export default function Home(){
         <div>
             <Header style={{position:'fixed'}}/>
                 <div>
-                    <div >
-                        <img class="d-block mx-auto fondo" src={Fondo} alt="First slide"/>
+                    <div style={{textalign:'justify'}} >
+                        <img class="fondo" src={Fondo} alt="First slide"/>
                             <h1>Andres  Garcia </h1>
-                            <p>Soy Web Developer egresado de Holberton School. Con un año de experiencia en desarrollo de software logré dominar la complejidad del lenguaje de programación C, Python, Javascript y pude mejorar mis softskills para el trabajo en equipos multidiciplinarios.
+                            <p >Soy Web Developer egresado de Holberton School. Con un año de experiencia en desarrollo de software logré dominar la complejidad del lenguaje de programación C, Python, Javascript y pude mejorar mis softskills para el trabajo en equipos multidiciplinarios.
                                 <br/>
                             Desde el perfil de desarrollador de Back-end y con el apoyo del equipo de partners, persistencia, continuidad y perseverancia, aprendí claramente los Fundamentos de la programación y desarrolle proyectos como la creación de la función Printf, un interprete de comandos Simple Shell, un interpretar de comandos  Monty Shell, Web Scraping con Python y un clon de la plataforma AirBnB. 
                             </p>
-                            <p><a className="btn btn-primary btn-lg" href="https://drive.google.com/file/d/1j7z_Tw5_Wy0Sv0FD8OXqiSuDqWt5L6PE/view?usp=sharing" rel="noopener noreferrer" target="_blank" role="button" padding='10px'>CV</a></p> 
+                            <p><a className="donwload" href="https://drive.google.com/file/d/1j7z_Tw5_Wy0Sv0FD8OXqiSuDqWt5L6PE/view?usp=sharing"  >CV</a></p> 
                     </div>
                 </div>
             <Footer />
