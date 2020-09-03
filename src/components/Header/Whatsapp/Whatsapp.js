f69c6684e77338a10e31283ae98d453c59d6e93b
andres0191 <andres_felipe0191@hotmail.com>
2020-09-03 11:39:44 -0500

    update favicon, description and name of page

--- src/components/Header/Whatsapp/Whatsapp.js
+++ src/components/Header/Whatsapp/Whatsapp.js
@@ -5,7 +5,7 @@ import { IconContext } from "react-icons";
 
 export default function Whatsapp(){
     return(
-        <div className='d-flex align-items-center' >
+        <div className='d-block align-items-center'>
             <IconContext.Provider value={{ color:"#bcbcbc", className: "global-class-name",  size:'1.5em'}} >
                 <div >
                     <p className="float-right"><a href="https://wa.me/573054214488" style={{margin: 10}}><ImWhatsapp /></a></p>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,18 +15,21 @@ export default function Home(){
                     <div >
                         <img class="fondo" src={Fondo} alt="First slide"/>
                         <div style={{textAlign:'justify', padding:'15px', paddingBottom: '5rem' }}>
-                            <img class="me" src={Me} alt="First slide"/>
-                            <h1>Andres  Garcia </h1>
-                            <p >Web Developer egresado de Holberton School. Con un año de experiencia en desarrollo de software logré dominar la complejidad del lenguaje de programación C, Python, Javascript y pude mejorar mis softskills para el trabajo en equipos multidiciplinarios.
-                                <br/><br/>
-                            Desde el perfil de desarrollador de Back-end y con el apoyo del equipo de partners, persistencia, continuidad y perseverancia, aprendí claramente los Fundamentos de la programación y desarrolle proyectos como la creación de la función Printf, un interprete de comandos Simple Shell, un interpretar de comandos  Monty Shell, Web Scraping con Python y un clon de la plataforma AirBnB. 
-                            </p><br/>
-                            <div className='d-flex align-items-center' >
-                                <IconContext.Provider value={{ color:"#bcbcbc", className: "global-class-name",  size:'1.5em'}} >
-                                    <div >
-                                        <p className="float-right"><a href="https://drive.google.com/file/d/1j7z_Tw5_Wy0Sv0FD8OXqiSuDqWt5L6PE/view?usp=sharing" style={{margin: 10}}><GrDocumentPdf /></a></p>
-                                    </div>
-                                </IconContext.Provider>
+                        <img class="me" src={Me} alt="First slide"/>
+                            <div className='text'>
+                                
+                                <h1>Andres  Garcia </h1>
+                                <p >Web Developer egresado de Holberton School. Con un año de experiencia en desarrollo de software logré dominar la complejidad del lenguaje de programación C, Python, Javascript y pude mejorar mis softskills para el trabajo en equipos multidiciplinarios.
+                                    <br/><br/>
+                                Desde el perfil de desarrollador de Back-end y con el apoyo del equipo de partners, persistencia, continuidad y perseverancia, aprendí claramente los Fundamentos de la programación y desarrolle proyectos como la creación de la función Printf, un interprete de comandos Simple Shell, un interpretar de comandos  Monty Shell, Web Scraping con Python y un clon de la plataforma AirBnB. 
+                                </p><br/>
+                                <div className='downloadcv' >
+                                    <IconContext.Provider value={{ color:"#bcbcbc", className: "global-class-name",  size:'1.5em'}} >
+                                        <div >
+                                            <p className="float-right">Download CV<a href="https://drive.google.com/file/d/1j7z_Tw5_Wy0Sv0FD8OXqiSuDqWt5L6PE/view?usp=sharing" style={{margin: 10}}><GrDocumentPdf /></a></p>
+                                        </div>
+                                    </IconContext.Provider>
+                                </div>  
                             </div>
                         </div>
                     </div>
@@ -35,11 +38,3 @@ export default function Home(){
         </div>
     )
 }
-/* GrDocumentPdf
-<div className='d-flex align-items-center' >
-            <IconContext.Provider value={{ color:"#bcbcbc", className: "global-class-name",  size:'1.5em'}} >
-                <div >
-                    <p className="float-right"><a href="https://drive.google.com/file/d/1j7z_Tw5_Wy0Sv0FD8OXqiSuDqWt5L6PE/view?usp=sharing" style={{margin: 10}}><FaMediumM /></a></p>
-                </div>
-            </IconContext.Provider>
-        </div> */
