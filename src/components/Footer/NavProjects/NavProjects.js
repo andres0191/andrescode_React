84901f1fddd731b82c38f91bec9dccf414c5aa95
andres0191 <andres_felipe0191@hotmail.com>
2020-09-04 16:20:22 -0500

    update projects page

--- src/components/Footer/NavProjects/NavProjects.js
+++ src/components/Footer/NavProjects/NavProjects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../Header/style.scss';
-import { BiCodeAlt } from "react-icons/bi";
+import { FaRegFileCode } from "react-icons/fa";
 import { IconContext } from "react-icons";
 
 export default function Linkedin(){
@@ -8,7 +8,7 @@ export default function Linkedin(){
         <div className='d-flex align-items-center'>
             <IconContext.Provider value={{ color:"#bcbcbc", className: "global-class-name", size:'1em', size:'1.5em'}} >
                 <div>
-                    <p className="float-right iconcenter"><a href="/Projects" style={{margin: 10}}><BiCodeAlt /></a></p>
+                    <p className="float-right iconcenter"><a href="/Projects" style={{margin: 10}}><FaRegFileCode /></a></p>
                 </div>
             </IconContext.Provider>
         </div>
