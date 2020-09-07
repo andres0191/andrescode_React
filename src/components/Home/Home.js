e35208c537872b62f8ec940c1647245998bc7da6
andres0191 <andres_felipe0191@hotmail.com>
2020-09-07 12:58:19 -0500

    update landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,13 +18,13 @@ export default function Home(){
                         <img class="me" src={Me} alt="First slide"/>
                             <div className='text'>
                                 <h1>Andres  Garcia </h1>
-                                <p >Web Developer graduated from Holberton School. With a year of experience in software development I managed to master the complexity of the programming language C, Python, Javascript and I was able to improve my softskills for working in multidisciplinary teams.
-                                    <br/><br/>
-                                From the Back-end developer profile and with the support of the partner team, persistence, continuity and perseverance, I clearly learned the Fundamentals of programming and developed projects such as the creation of the Printf function, a Simple Shell command interpreter, a interpret command Monty Shell, Web Scraping with Python and a clone of the AirBnB platform. 
+                                <p >Web Developer graduated from Holberton School. With a year of experience in software development developing projects in programming languages ​​such as C, Python, Javascript and I was able to improve my softskills to work in multidisciplinary teams.
+                                <br/><br/>
+                                From the back-end developer profile and with the support of the partner team, persistence, continuity and perseverance, I clearly learned the Fundamentals of programming and developed projects such as the creation of the Printf function, a simple Shell command interpreter, a Monty Shell interpretation command, Web Scraping with Python and a clone of the AirBnB platform. In my last project I learned the development of mobile applications with React Native, Redux, Firebase and NodeJs.
                                 </p><br/>
                                 <div className='downloadcv' >
                                     <IconContext.Provider value={{ color:"#bcbcbc", className: "global-class-name",  size:'1.5em'}} >
-                                        <div >
+                                        <div className=' d-flex justify-content-center mt-3' >
                                             <p className="float-right">Download CV<a href="https://drive.google.com/file/d/1j7z_Tw5_Wy0Sv0FD8OXqiSuDqWt5L6PE/view?usp=sharing" style={{margin: 10}}><GrDocumentPdf /></a></p>
                                         </div>
                                     </IconContext.Provider>
