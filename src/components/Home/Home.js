6a4d45a10b1767a049c3be0ab04742eada109944
andres0191 <andres_felipe0191@hotmail.com>
2020-09-08 11:42:09 -0500

    update description

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -20,7 +20,7 @@ export default function Home(){
                                 <h1>Andres  Garcia </h1>
                                 <p >Web Developer graduated from Holberton School. With a year of experience in software development developing projects in programming languages ​​such as C, Python, Javascript and I was able to improve my softskills to work in multidisciplinary teams.
                                 <br/><br/>
-                                From the back-end developer profile and with the support of the partner team, persistence, continuity and perseverance, I clearly learned the Fundamentals of programming and developed projects such as the creation of the Printf function, a simple Shell command interpreter, a Monty Shell interpretation command, Web Scraping with Python and a clone of the AirBnB platform. In my last project I learned the development of mobile applications with React Native, Redux, Firebase and NodeJs.
+                                From the web developer profile and with persistence, continuity and perseverance, I clearly learned the Fundamentals of programming and developed projects such as the creation of the Printf function, a simple Shell command interpreter, Web Scraping with Python and a clone of the platform AirBnB. In my last project and thanks to the skills I have to work as a Frontend, I learned web development with React and mobile application development with React Native, Redux, Firebase and NodeJs.
                                 </p><br/>
                                 <div className='downloadcv' >
                                     <IconContext.Provider value={{ color:"#bcbcbc", className: "global-class-name",  size:'1.5em'}} >
